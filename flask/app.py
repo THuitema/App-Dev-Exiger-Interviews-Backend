44fa0db182842f3d39c7da202a93bf5755b53bed
Thomas Huitema <thuitema35@gmail.com>
2025-09-14 16:31:02 -0400

    Create boilerplate endpoints for flask

--- flask/app.py
+++ flask/app.py
@@ -3,10 +3,22 @@ To run:
 1. cd flask
 2. flask run OR python -m flask run
 """
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<p>Hello, World!</p>'
+@app.route('/movies')
+def add_movie():
+    return 200, jsonify({})
+
+@app.route('/movies')
+def get_movies():
+    return 200, jsonify({})
+
+@app.route('/movies')
+def search_by_title():
+    return 200, jsonify({})
+
+@app.route('/movies/<movie_id>/rating')
+def update_rating(movie_id):
+    return 200, jsonify({})
