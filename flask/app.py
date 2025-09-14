69abbaf34722fb65361ed99de31d22d17ff09ccb
Thomas Huitema <thuitema35@gmail.com>
2025-09-14 16:34:34 -0400

    Update boilerplate for flask

--- flask/app.py
+++ flask/app.py
@@ -3,22 +3,26 @@ To run:
 1. cd flask
 2. flask run OR python -m flask run
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/movies')
+# Create your data structure(s) here:
+
+# Complete the API endpoints below:
+
+@app.route('/movies', methods=['POST'])
 def add_movie():
     return 200, jsonify({})
 
-@app.route('/movies')
+@app.route('/movies', methods=['GET'])
 def get_movies():
     return 200, jsonify({})
 
-@app.route('/movies')
+@app.route('/movies', methods=['GET'])
 def search_by_title():
     return 200, jsonify({})
 
-@app.route('/movies/<movie_id>/rating')
+@app.route('/movies/<movie_id>/rating', methods=['PUT'])
 def update_rating(movie_id):
     return 200, jsonify({})
